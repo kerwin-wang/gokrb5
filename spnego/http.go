c79889b07f400b52216348a9d081db5e7ac32873
hankkhan <hankkhan@tencent.com>
2022-01-26 14:20:44 +0800

    fix：修复krb认证，超过1m数据上传hdfs失败

--- spnego/http.go
+++ spnego/http.go
@@ -75,12 +75,18 @@ func NewClient(krb5Cl *client.Client, httpCl *http.Client, spn string) *Client {
 
 // Do is the SPNEGO enabled HTTP client's equivalent of the http.Client's Do method.
 func (c *Client) Do(req *http.Request) (resp *http.Response, err error) {
-	var body bytes.Buffer
+	//var body bytes.Buffer
+	//if req.Body != nil {
+	//	// Use a tee reader to capture any body sent in case we have to replay it again
+	//	teeR := io.TeeReader(req.Body, &body)
+	//	teeRC := teeReadCloser{teeR, req.Body}
+	//	req.Body = teeRC
+	//}
+	var body []byte
 	if req.Body != nil {
 		// Use a tee reader to capture any body sent in case we have to replay it again
-		teeR := io.TeeReader(req.Body, &body)
-		teeRC := teeReadCloser{teeR, req.Body}
-		req.Body = teeRC
+		body, _ := ioutil.ReadAll(req.Body)
+		req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 	}
 	resp, err = c.Client.Do(req)
 	if err != nil {
@@ -94,7 +100,7 @@ func (c *Client) Do(req *http.Request) (resp *http.Response, err error) {
 				}
 				if req.Body != nil {
 					// Refresh the body reader so the body can be sent again
-					e.reqTarget.Body = ioutil.NopCloser(&body)
+					e.reqTarget.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 				}
 				return c.Do(e.reqTarget)
 			}
@@ -108,7 +114,7 @@ func (c *Client) Do(req *http.Request) (resp *http.Response, err error) {
 		}
 		if req.Body != nil {
 			// Refresh the body reader so the body can be sent again
-			req.Body = ioutil.NopCloser(&body)
+			req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 		}
 		return c.Do(req)
 	}
